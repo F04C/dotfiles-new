8acf9c31c6d397edb1857e6cbed07fd46a1615bf
gustavomarchesim <gustavomarchesim@outlook.com>
2024-07-16 02:22:20 -0300

    added Waybar

---
+++ .config/waybar/scripts/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ----------------------------------------------------- 
+# Quit all running waybar instances
+# ----------------------------------------------------- 
+killall waybar
+pkill waybar
+sleep 0.2
+
+waybar
+
+
