70c29b52e824cca89aeb2bcb5930f8b200e8fee3
gustavomarchesim <gustavomarchesim@outlook.com>
2024-07-16 02:18:12 -0300

    added in Hyprland

---
+++ .config/hypr/scripts/xdg.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Espera 1 segundo para garantir que todos os serviços estão prontos para serem parados
+sleep 1
+
+# Mata todos os processos relacionados ao xdg-desktop-portal
+killall xdg-desktop-portal-hyprland
+killall xdg-desktop-portal-gtk
+killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal-gnome
+killall xdg-desktop-portal
+
+# Espera 1 segundo para garantir que todos os processos foram terminados
+sleep 1
+
+# Inicia o xdg-desktop-portal-hyprland
+if command -v /usr/lib/xdg-desktop-portal-hyprland &> /dev/null
+then
+    /usr/lib/xdg-desktop-portal-hyprland &
+elif command -v /usr/libexec/xdg-desktop-portal-hyprland &> /dev/null
+then
+    /usr/libexec/xdg-desktop-portal-hyprland &
+fi
+
+# Espera 2 segundos para garantir que o xdg-desktop-portal-hyprland iniciou corretamente
+sleep 2
+
+# Inicia o xdg-desktop-portal-wlr
+if command -v /usr/lib/xdg-desktop-portal-wlr &> /dev/null
+then
+    /usr/lib/xdg-desktop-portal-wlr &
+elif command -v /usr/libexec/xdg-desktop-portal-wlr &> /dev/null
+then
+    /usr/libexec/xdg-desktop-portal-wlr &
+fi
+
+# Espera 2 segundos para garantir que o xdg-desktop-portal-wlr iniciou corretamente
+sleep 2
+
+# Inicia o xdg-desktop-portal-gtk
+if command -v /usr/lib/xdg-desktop-portal-gtk &> /dev/null
+then
+    /usr/lib/xdg-desktop-portal-gtk &
+elif command -v /usr/libexec/xdg-desktop-portal-gtk &> /dev/null
+then
+    /usr/libexec/xdg-desktop-portal-gtk &
+fi
+
+# Espera 2 segundos para garantir que o xdg-desktop-portal-gtk iniciou corretamente
+sleep 2
+
+# Inicia o xdg-desktop-portal
+if command -v /usr/lib/xdg-desktop-portal &> /dev/null
+then
+    /usr/lib/xdg-desktop-portal &
+elif command -v /usr/libexec/xdg-desktop-portal &> /dev/null
+then
+    /usr/libexec/xdg-desktop-portal &
+fi
+
+# Espera 2 segundos para garantir que todos os serviços foram iniciados corretamente
+sleep 2
