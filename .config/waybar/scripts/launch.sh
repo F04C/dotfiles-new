fa6ff6ff9d298f99f9a1abe736c1e7756a7e32a2
gustavomarchesim <gustavomarchesim@outlook.com>
2024-07-18 20:32:41 -0300

    Update

--- .config/waybar/scripts/launch.sh
+++ .config/waybar/scripts/launch.sh
@@ -9,4 +9,16 @@ sleep 0.2
 
 waybar
 
+[ -f "$HOME/.cache/wal/colors.sh" ] && . "$HOME/.cache/wal/colors.sh"
+
+pidof dunst && killall dunst
+
+DUNST_FILE=~/.config/dunst/dunstrc
+
+# update dunst based on pywal colors.
+sed -i '/background = /s/.*/    background = "'$color0'"/' $DUNST_FILE
+sed -i '/foreground = /s/.*/    foreground = "'$color2'"/' $DUNST_FILE
+sed -i '/frame_color = /s/.*/    frame_color = "'$color5'"/' $DUNST_FILE
+
+dunst -config ~/.config/dunst/dunstrc > /dev/null 2>&1 &
 
