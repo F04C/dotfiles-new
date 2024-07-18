a8b206302b0a3d426fb032d2a2423d13f613c899
gustavomarchesim <gustavomarchesim@outlook.com>
2024-07-17 23:21:05 -0300

    refact: muda o script de limpeza e adiciona um novo script

--- scripts/limpeza.sh
+++ scripts/limpeza.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 clear
-
-# Cabeçalho ASCII
 cat <<"EOF"
   ____ _                                
  / ___| | ___  __ _ _ __    _   _ _ __  
@@ -9,38 +7,9 @@ cat <<"EOF"
 | |___| |  __/ (_| | | | | | |_| | |_) |
  \____|_|\___|\__,_|_| |_|  \__,_| .__/ 
                                  |_|    
+
 EOF
 
-sudo -v
+sudo pacman -Rns $(pacman -Qtdq)
 
-# Limpa o cache do pacman
-echo "Limpando o cache do pacman..."
-yes | sudo pacman -Sc
-sleep 2
-
-echo "Limpando o cache do yay..."
-yes | yay -Sc
-sleep 2
-
-echo "Limpando o cache do paru..."
-yes | paru -Sc
-sleep 2
-
-echo "Removendo dependências órfãs com pacman..."
-yes | sudo pacman -Rns $(pacman -Qtdq)
-sleep 2
-
-#echo "Removendo dependências órfãs com yay..."
-#yes | yay -Qdtq | yay -Rns -
-#sleep 2
-
-echo "Removendo dependências órfãs com paru..."
-yes | paru -Qdtq | paru -Rns -
-sleep 2
-
-# Limpeza do cache do paru (AUR)
-echo "Limpando o cache do paru..."
-yes | paru -Scc
-sleep 2
-w
-echo "Limpeza concluída."
+yay -Scc
