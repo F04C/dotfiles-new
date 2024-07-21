4bb527f762d4670aecf388a7009b86bf38d0e28a
gustavomarchesim <gustavomarchesim@outlook.com>
2024-07-21 14:25:48 -0300

    Ajuste no Script do GTK

--- .config/hypr/scripts/gtk.sh
+++ .config/hypr/scripts/gtk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash   
 # Source: https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
 
-config="$HOME/.config/gtk-3.0/settings.ini"
+config="${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/settings.ini"
 if [ ! -f "$config" ]; then exit 1; fi
 
 gnome_schema="org.gnome.desktop.interface"
@@ -10,12 +10,6 @@ icon_theme="$(grep 'gtk-icon-theme-name' "$config" | sed 's/.*\s*=\s*//')"
 cursor_theme="$(grep 'gtk-cursor-theme-name' "$config" | sed 's/.*\s*=\s*//')"
 cursor_size="$(grep 'gtk-cursor-theme-size' "$config" | sed 's/.*\s*=\s*//')"
 font_name="$(grep 'gtk-font-name' "$config" | sed 's/.*\s*=\s*//')"
-
-echo $gtk_theme
-echo $icon_theme
-echo $cursor_theme
-echo $cursor_size
-echo $font_name
 
 gsettings set "$gnome_schema" gtk-theme "$gtk_theme"
 gsettings set "$gnome_schema" icon-theme "$icon_theme"
