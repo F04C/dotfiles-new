ce1e5e62b64ca3b9d1e03a9896ab0c4e0b6f4c87
gustavomarchesim <gustavomarchesim@outlook.com>
2024-07-22 16:34:40 -0300

    Corrige o bug no Toggle da Waybar

--- .config/waybar/scripts/toggle.sh
+++ .config/waybar/scripts/toggle.sh
@@ -5,9 +5,7 @@ if pgrep -x "waybar" > /dev/null
 then
     # Se estiver rodando, envia o sinal SIGTERM para encerrar o Waybar
     killall -q waybar
-    echo "Waybar encerrado."
 else
     # Se não estiver rodando, inicia o Waybar
     waybar &
-    echo "Waybar iniciado."
 fi
